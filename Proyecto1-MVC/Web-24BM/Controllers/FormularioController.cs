f65877edff57382cf3bc51be8bc095277a9fa80b
Christopher <soyronyvargas@gmail.com>
2023-10-13 13:51:47 -0500

    feat: nuevo controlador

---
+++ Proyecto1-MVC/Web-24BM/Controllers/FormularioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_24BM.Services;
+
+namespace Web_24BM.Controllers
+{
+    public class FormularioController : Controller
+    {
+        public readonly IEmailSenderService emailSenderService;
+
+        public FormularioController(IEmailSenderService emailSenderService)
+        {
+            this.emailSenderService = emailSenderService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
